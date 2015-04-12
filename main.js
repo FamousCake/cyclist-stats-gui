188344dd7e080ff9c0c0048750741be3555e7069
FamousCake <peter.g.hristov@gmail.com>
2015-04-12 09:32:38 +0100

    Added and configured chart js libraray

--- main.js
+++ main.js
@@ -1,89 +1,103 @@
-var module = function(filepath) {
+var module = function(filepath, callback) {
+
+    var _ = require('underscore');
+    var addon = require('bindings')('addon.node');
+
+    var hrmData = addon(filepath);
 
     document.getElementById('main').innerHTML = '';
-
-    var addon = require('bindings')('addon.node');
-    var _ = require('underscore');
-
-    var gosho = addon(filepath);
-
     var text = '';
 
-    text += '<div> Average heart rate is : ' + (gosho.HeartRate / gosho.Count).toFixed(2) + ' BPM </div>';
-    text += '<div> Average speed is : ' + (gosho.Speed / gosho.Count).toFixed(2) + ' KM/H </div>';
-    text += '<div> Average cadence is : ' + (gosho.Cadance / (gosho.Count * 10)).toFixed(2) + ' RPM </div>';
+    text += '<div> The software version is : ' + hrmData.Version + '</div>';
+    text += '<div> The monitor is : ' + hrmData.Monitor + '</div>';
 
-    text += '<div> Average altitude is : ' + (gosho.Altitude / gosho.Count).toFixed(2) + ' feet </div>';
-    text += '<div> Average power is : ' + (gosho.Power / gosho.Count).toFixed(2) + ' W </div>';
-    text += '<div> Average power balance is : ' + (gosho.PowerBalance / gosho.Count ).toFixed(2) + '</div>';
-
-    text += '<div> The version is : ' + gosho.Version + '</div>';
-
-    text += '<div> Array : ';
-
-    _.each(gosho.TestArray, function(item){
-        text += item + ', ';
-
-    });
-
-    text += '</div>';
-
-
-    text += '<div> Array : ';
-
-    _.each(gosho.HeartRateData, function(item){
-        text += item + ' | ';
-
-    });
-
-    text += '</div>';
-
-
-    text += '<div> Array : ';
-
-    _.each(gosho.SpeedData, function(item){
-        text += item + ' | ';
-
-    });
-
-    text += '</div>';
-    text += '<div> Array : ';
-
-    _.each(gosho.CadanceData, function(item){
-        text += item + ' | ';
-
-    });
-
-    text += '</div>';
-    text += '<div> Array : ';
-
-    _.each(gosho.AltitudeData, function(item){
-        text += item + ' | ';
-
-    });
-
-    text += '</div>';
-    text += '<div> Array : ';
-
-    _.each(gosho.PowerData, function(item){
-        text += item + ' | ';
-
-    });
-
-    text += '</div>';
-    text += '<div> Array : ';
-
-    _.each(gosho.PowerBalanceData, function(item){
-        text += item + ' | ';
-
-    });
-
-    text += '</div>';
-
-
-
-
+    text += '<div> The date is : ' + hrmData.Date + '</div>';
+    text += '<div> The starting time is : ' + hrmData.StartTime + '</div>';
 
     document.getElementById('main').innerHTML += text;
 
+    // document.getElementById('main').innerHTML = '';
+
+    // var text = '';
+
+    // text += '<div> Average heart rate is : ' + (hrmData.HeartRate / hrmData.Count).toFixed(2) + ' BPM </div>';
+    // text += '<div> Average speed is : ' + (hrmData.Speed / hrmData.Count).toFixed(2) + ' KM/H </div>';
+    // text += '<div> Average cadence is : ' + (hrmData.Cadance / (hrmData.Count * 10)).toFixed(2) + ' RPM </div>';
+
+    // text += '<div> Average altitude is : ' + (hrmData.Altitude / hrmData.Count).toFixed(2) + ' feet </div>';
+    // text += '<div> Average power is : ' + (hrmData.Power / hrmData.Count).toFixed(2) + ' W </div>';
+    // text += '<div> Average power balance is : ' + (hrmData.PowerBalance / hrmData.Count ).toFixed(2) + '</div>';
+
+    // text += '<div> The version is : ' + hrmData.Version + '</div>';
+
+    // text += '<div> Array : ';
+
+    // _.each(hrmData.TestArray, function(item){
+    //     text += item + ', ';
+
+    // });
+
+    // text += '</div>';
+
+
+    // text += '<div> Array : ';
+
+    // _.each(hrmData.HeartRateData, function(item){
+    //     text += item + ' | ';
+
+    // });
+
+    // text += '</div>';
+
+
+    // text += '<div> Array : ';
+
+    // _.each(hrmData.SpeedData, function(item){
+    //     text += item + ' | ';
+
+    // });
+
+    // text += '</div>';
+    // text += '<div> Array : ';
+
+    // _.each(hrmData.CadanceData, function(item){
+    //     text += item + ' | ';
+
+    // });
+
+    // text += '</div>';
+    // text += '<div> Array : ';
+
+    // _.each(hrmData.AltitudeData, function(item){
+    //     text += item + ' | ';
+
+    // });
+
+    // text += '</div>';
+    // text += '<div> Array : ';
+
+    // _.each(hrmData.PowerData, function(item){
+    //     text += item + ' | ';
+
+    // });
+
+    // text += '</div>';
+    // text += '<div> Array : ';
+
+    // _.each(hrmData.PowerBalanceData, function(item){
+    //     text += item + ' | ';
+
+    // });
+
+    // text += '</div>';
+
+
+
+
+
+    // document.getElementById('main').innerHTML += text;
+
+    callback(hrmData);
+
+    //return hrmData;
 };
